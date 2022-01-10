3b3ba8ab19c18c7aa646aafc809a9956936f4caa
EmixamG <93671363+EmixamG@users.noreply.github.com>
2022-01-10 13:51:56 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+var fields = {};
+
+document.addEventListener("DOMContentLoaded", function() {
+    fields.firstName = document.getElementById('firstName');
+    fields.lastName = document.getElementById('lastName');
+    fields.email = document.getElementById('email');
+    fields.message = document.getElementById('message');
+    fields.websiteRequest = document.getElementById('websiteRequest');
+    fields.marketingRequest = document.getElementById('marketingRequest');
+    fields.contentRequest = document.getElementById('contentRequest');
+    fields.other = document.getElementById('other');
+    fields.accept = document.getElementById('accept');
+   })
+
